4d13eb3ae1961c87aac7067b5159e52ac5ff0834
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-03-10 10:14:15 +0530

    Create Missing Number.py

---
+++ Arrays/Missing Number.py
@@ -0,0 +1,33 @@
+"""
+https://practice.geeksforgeeks.org/problems/missing-number-in-array1416/1#
+"""
+#1.Missing number
+-->Approach 1 (Sum formula):
+
+def MissingNumber(array,n):
+    totalSum = ((n)*(n+1))//2
+    return totalSum-sum(array)
+
+#-->Approach 2 (Hashing):
+
+def MissingNumber(array,n):
+
+    numberDict = {i:False for i in range(1,len(array)+2)}
+    for element in array:
+        if element in numberDict.keys():
+            numberDict[element] = True
+    for key,value in numberDict.items():
+        if value == False:
+            return key
+
+#-->Approach 3 (Xor method):
+
+def getMissingNo(a, n):
+    x1 = a[0]
+    x2 = 1
+    for i in range(1,len(a)):
+        x1 = x1^a[i]
+    for j in range(2,len(a)+2):
+        x2 = x2^j
+
+    return x1^x2
