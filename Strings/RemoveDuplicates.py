7c0be8aff1b264f98294c085e59ce9ec98d60999
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-03-19 21:31:27 +0530

    Create RemoveDuplicates.py

---
+++ Strings/RemoveDuplicates.py
@@ -0,0 +1,21 @@
+#TC:O(|S|)
+#SC:O(|S|)
+def removeDups(self, S):
+        charList = []
+        for i in S:
+            if i not in charList:
+                charList.append(i)
+        return "".join(map(str,charList))
+-----------------------------------------
+#TC:O(|S|)
+#SC:O(|S|)
+
+from collections import OrderedDict
+class Solution:
+    def removeDups(self, S):
+        od = OrderedDict()
+
+         for i in S:
+            od[i] = "" 
+
+        return ''.join(list(od.keys()))
