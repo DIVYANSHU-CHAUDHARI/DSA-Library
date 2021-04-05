0986def3d26544665d88e683a4ccbdb600809596
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-04-05 08:52:32 +0530

    Update Determine if Two Trees are Identical.py

--- Trees/Determine if Two Trees are Identical.py
+++ Trees/Determine if Two Trees are Identical.py
@@ -3,8 +3,11 @@ https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical
 '''
 
 def isIdentical(self,root1, root2):
+        #Checking if both trees are empty
         if root1 == None and root2 == None:
             return True
+        #Checking if one of the trees is non-empty
         if root1 == None or root2==None:
             return False
+
         return (root1.data == root2.data) and self.isIdentical(root1.left,root2.left) and self.isIdentical(root1.right,root2.right)
