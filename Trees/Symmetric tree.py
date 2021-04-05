38697043a36b15451cfd0bdc61ff1147d399a057
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-04-05 11:05:37 +0530

    Update Symmetric tree.py

--- Trees/Symmetric tree.py
+++ Trees/Symmetric tree.py
@@ -1,4 +1,4 @@
- 
+ #Recursive apprach
 def isMirror(root1,root2):
     if root1==None and root2==None:
         return True
@@ -9,3 +9,27 @@ def isMirror(root1,root2):
 def isSymmetric(root):
     
     return isMirror(root,root)
+  ###################################################
+  
+  #Iterative approach
+  
+      def isSymmetric(self, root):
+        if root==None:
+            return True
+        queue = [root.left,root.right]
+        while queue!=[]:
+            temp_left = queue.pop(0)
+            temp_right = queue.pop(0)
+            
+            if temp_left==None and temp_right == None:
+                continue
+            if temp_left==None or temp_right == None:
+                return False
+            if temp_left.val!=temp_right.val:
+                return False
+            queue.append(temp_left.left)
+            queue.append(temp_right.right)
+            queue.append(temp_left.right)
+            queue.append(temp_right.left)
+        return True
+                     
