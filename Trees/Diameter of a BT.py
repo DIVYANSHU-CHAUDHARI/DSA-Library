9497f6f37591a9dc3bf57676f08f85539ffd0882
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-04-06 08:51:25 +0530

    Update Diameter of a BT.py

--- Trees/Diameter of a BT.py
+++ Trees/Diameter of a BT.py
@@ -5,12 +5,16 @@
         maxLength = float('-inf') 
         
         #diameterHelper returns the maxlength from root node to any of the leaf nodes reachable from that root node.
+        #This is basically the height of the root node
+        
+        #I can rename diameterHelper to height here
         
         def diameterHelper(root):
             nonlocal maxLength
             if root == None:return 0
             
             #Max length from its left and right childs to their leaf nodes is calculated
+            #This is basically the height of its left and right child
             leftMaxLength = diameterHelper(root.left)
             rightMaxLength = diameterHelper(root.right)
             
