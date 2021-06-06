e53c89d5de05251f134055c3c1a7703b7048f865
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-06-06 18:58:47 +0530

    Update print-all-primes-till-n.java

--- Arrays/print-all-primes-till-n.java
+++ Arrays/print-all-primes-till-n.java
@@ -1,18 +1,19 @@
 import java.util.*;
-
-public class Main{
-    public static void main(String[] args) {
-        Scanner scn =new Scanner(System.in);
-        int low = scn.nextInt();
-        int high = scn.nextInt();
-        for(int n=low;n<=high;n++){
-            int factors = 0;
-            for(int i=2;i*i<=n;i++){
-                if(n%i==0){
-                    factors++;
-                    break;
-                }
-            }
-            if(factors==0){
-                System.out.println(n);
-            }
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int a = 0;
+      int b = 1;
+      System.out.println(a);
+      System.out.println(b);
+      for(int i = 0;i<n-2;i++){
+          int next = a + b;
+          System.out.println(next);
+          a = b;
+          b = next;
+      }
+   }
+  }
