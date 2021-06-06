15867470e1fb51413b063e3d2070bf802eba3b7f
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-06-06 20:16:04 +0530

    Create Digits of a number.java

---
+++ Arrays/Digits of a number.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+    
+    public class Main{
+    
+    public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int temp_n = n;
+ //Counting the no. of digits in the numbers because we want to start by dividing the no. by 10^(d-1) where d = no. of digits in the number
+      int count = 0;
+      while(temp_n!=0){
+          count++;
+          temp_n/=10;
+      }
+      int div = (int)Math.pow(10,count-1);
+      //Math.pow returns a double,This is typecasted into int.
+      while(div!=0){
+          int quotient = n/div;
+          System.out.println(quotient);
+          int rem = n%div;
+          n = rem;
+          div/=10;
+      }
+     }
+    }
