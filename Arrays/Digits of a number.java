b98c24360c97f685ee6a76233eda22a282a24ffe
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-06-06 20:29:38 +0530

    Update Digits of a number.java

--- Arrays/Digits of a number.java
+++ Arrays/Digits of a number.java
@@ -7,7 +7,8 @@ import java.util.*;
       Scanner scn = new Scanner(System.in);
       int n = scn.nextInt();
       int temp_n = n;
- //Counting the no. of digits in the numbers because we want to start by dividing the no. by 10^(d-1) where d = no. of digits in the number
+ //Counting the no. of digits in the numbers because we want to start dividing the no. by 10^(d-1) where d = no. of digits in the number
+ //This is done to get the quotient because we want to start printing from the start.
       int count = 0;
       while(temp_n!=0){
           count++;
