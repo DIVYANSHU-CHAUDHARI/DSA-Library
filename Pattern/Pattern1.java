8f214e09f1ecd1219c8daec265d27bdb35e8c153
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-06-10 22:14:57 +0530

    Update Pattern1.java

--- Pattern/Pattern1.java
+++ Pattern/Pattern1.java
@@ -1,5 +1,6 @@
 //Consider outer loop as row and inner loop as column
 //After inner loop gets completed, use System.out.println("") to enter(go to next line)
+//ith row contains i stars
 
 import java.util.*;
 
@@ -8,8 +9,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        for(int i =0;i<n;i++){
-            for(int j=0;j<i+1;j++){
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i;j++){
                 System.out.print("*\t");
             }
             System.out.println("");
