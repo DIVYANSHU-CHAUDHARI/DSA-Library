fb2af1d6834395d5c878b6f68f5531d83c3fb000
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-06-14 09:06:51 +0530

    Update BFS of a graph.py

--- Graph/BFS of a graph.py
+++ Graph/BFS of a graph.py
@@ -1,19 +1,32 @@
   '''
   https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1#
   '''
-  
-  #Function to return Breadth First Traversal of given graph.
-    def bfsOfGraph(self, V, adj):
-        traversal = []
-        visited = [False]*V
-        at = 0
-        queue= [at]
-        visited[at] = True
-        while len(queue)!=0:
-            node = queue.pop(0)
-            traversal.append(node)
-            for element in adj[node]:
-                if not visited[element]:
-                    visited[element] =True
-                    queue.append(element)
-        return traversal
+ 
+class Solution
+{
+    //Function to return Breadth First Traversal of given  graph.
+    public ArrayList<Integer> bfsOfGraph(int V,ArrayList<ArrayList<Integer>> adj)
+    {
+        boolean[] visited = new boolean[V];
+        Queue<Integer>q = new LinkedList<Integer>();
+        //add the source elemenet in queue
+        q.add(0);
+        //Mark the element as visited
+        visited[0] = true;
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        while(q.size()!=0){
+            int u = q.poll();
+            arr.add(u);
+            //System.out.println(u+"\t");
+            for(int v:adj.get(u)){
+                if(!visited[v]){
+                    visited[v] = true;
+                    q.add(v);
+                }
+            }
+            
+        }
+        return arr;
+    }
+}
