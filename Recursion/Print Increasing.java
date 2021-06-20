969fd36fc1f20bd6147a08973289afaf2bf12e2c
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-06-20 20:48:21 +0530

    Create Print Increasing.java

---
+++ Recursion/Print Increasing.java
@@ -0,0 +1,20 @@
+//https://classroom.pepcoding.com/myClassroom/the-switch-program-3/introduction-to-recursion/print-increasing-official/ojquestion
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+        if(n==0)return;
+        printIncreasing(n-1);
+        System.out.println(n);
+        
+    }
+
+}
