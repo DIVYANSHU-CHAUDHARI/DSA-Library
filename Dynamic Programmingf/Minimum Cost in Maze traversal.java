c21771b39e6a29b775110d61b31362528f224d2e
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-07-08 21:40:08 +0530

    Update Minimum Cost in Maze traversal.java

--- Dynamic Programmingf/Minimum Cost in Maze traversal.java
+++ Dynamic Programmingf/Minimum Cost in Maze traversal.java
@@ -1,3 +1,4 @@
+//Memoized Version
 import java.io.*;
 import java.util.*;
 
@@ -34,6 +35,49 @@ public class Main {
         dp[sr][sc] = ans;
         return ans;
         
+    }
+
+}
+
+
+
+//Tabulation Method
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        System.out.println(minCostTab(arr));
+    }
+    
+    public static int minCostTab(int[][] arr){
+       int[][] dp = new int[arr.length][arr[0].length];
+       
+       for(int i = arr.length-1;i>=0;i--){
+           for(int j=arr[0].length-1;j>=0;j--){
+               if(i==arr.length-1 && j==arr[0].length-1){
+                   dp[i][j] = arr[i][j];
+               }else if(i==arr.length-1){
+                   dp[i][j] = dp[i][j+1]+arr[i][j];
+               }else if(j==arr[0].length-1){
+                   dp[i][j] = dp[i+1][j]+arr[i][j];
+               }else{
+                   dp[i][j] = Math.min(dp[i+1][j],dp[i][j+1])+arr[i][j];
+               }
+           }
+       }
+        return dp[0][0];
     }
 
 }
