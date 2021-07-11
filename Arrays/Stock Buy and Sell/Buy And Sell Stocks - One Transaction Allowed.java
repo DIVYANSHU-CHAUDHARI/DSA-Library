f258b5a9784bf8639f2ddcebd250bb38d0b182cd
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-07-11 19:29:12 +0530

    Update Buy And Sell Stocks - One Transaction Allowed.java

--- Arrays/Stock Buy and Sell/Buy And Sell Stocks - One Transaction Allowed.java
+++ Arrays/Stock Buy and Sell/Buy And Sell Stocks - One Transaction Allowed.java
@@ -1,3 +1,5 @@
+//Left to right interation updating maxprofit using minimum value find till now.
+
 import java.io.*;
 import java.util.*;
 
@@ -22,6 +24,34 @@ public class Main {
 
         }
         System.out.println(maximumProfit);
+    }
+
+}
+
+////right to left interation updating maxprofit using maximum value find till now.
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] prices = new int[n];
+        for(int i=0;i<prices.length;i++){
+            prices[i] = scn.nextInt();
+        }
+        int maxsofar = prices[n-1];
+        int maxprofit = Integer.MIN_VALUE;    
+        for(int i=prices.length-2;i>=0;i--){
+            if(prices[i]>maxsofar){
+                maxsofar = prices[i];
+            }else{
+                maxprofit = Integer.max(maxprofit,maxsofar-prices[i]);
+            }
+            
+        }
+        System.out.println(maxprofit);
     }
 
 }
