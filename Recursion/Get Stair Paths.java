7e99591c5e6a14270147cdf9b7a61ec9e718fd1b
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-07-21 17:53:05 +0530

    Create Get Stair Paths.java

---
+++ Recursion/Get Stair Paths.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.println(getStairPaths(n));
+    }
+
+    public static ArrayList<String> getStairPaths(int n) {
+        if(n==0){
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }else if(n<0){
+            ArrayList<String> base = new ArrayList<>();
+            return base;
+        }
+        ArrayList<String> list1 = getStairPaths(n-1);
+        ArrayList<String> list2 = getStairPaths(n-2);
+        ArrayList<String> list3 = getStairPaths(n-3);
+        ArrayList<String> ans = new ArrayList<>();
+        for(String str:list1){
+            ans.add("1" + str);
+        }
+        for(String str:list2){
+            ans.add("2"+str);
+        }
+        for(String str:list3){
+            ans.add("3" + str);
+        }
+        return ans;
+    }
+
+}
