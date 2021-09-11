121a214c137737cffaebe28d2cf5b8404420e442
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-09-11 16:48:26 +0530

    Update RemoveDuplicates.py

--- Strings/RemoveDuplicates.py
+++ Strings/RemoveDuplicates.py
@@ -1,24 +1,18 @@
 """
 https://practice.geeksforgeeks.org/problems/remove-duplicates3034/1#
 """
-#TC:O(|S|)
-#SC:O(|S|)
-def removeDups(self, S):
-        charList = []
-        for i in S:
-            if i not in charList:
-                charList.append(i)
-        return "".join(map(str,charList))
------------------------------------------
-#TC:O(|S|)
-#SC:O(|S|)
-
-from collections import OrderedDict
-class Solution:
-    def removeDups(self, S):
-        od = OrderedDict()
-
-         for i in S:
-            od[i] = "" 
-
-        return ''.join(list(od.keys()))
+class Solution {
+    String removeDups(String S) {
+        Set<Character> charSet = new LinkedHashSet<>();
+        for(int i=0;i<S.length();i++){
+            char c = S.charAt(i);
+            charSet.add(c);
+        }
+        String result = "";
+        for(char c:charSet){
+            result+=c;
+        }
+        return result;
+        
+    }
+}
