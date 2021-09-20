0fc8724049a66f0652484207e588b375d10f92b6
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-09-20 19:21:35 +0530

    Update and rename Remove Loop in Linked List.py to Remove Loop in Linked List.java

---
+++ LinkedList/Remove Loop in Linked List.java
@@ -0,0 +1,42 @@
+"""
+https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1#
+"""
+//Approach1:Check editorial Method 3
+//TC:O(n)
+//SC:O(1)
+    //Function to remove a loop in the linked list.
+class Solution
+{
+    //Function to remove a loop in the linked list.
+    public static void removeLoop(Node head){
+        // if(head==null || head.next!=null){
+        //     return;
+        // }
+        Node slow = head;
+        Node fast = head;
+        
+        while(fast!=null && fast.next!=null && slow!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast)break;
+        }   
+        
+        if(slow == head){
+            slow = head;
+            fast = head;
+            while(fast.next!=slow){
+                fast = fast.next;
+            }
+            fast.next = null;
+            
+        }else if(slow == fast){
+            slow = head;
+            while(slow.next!=fast.next){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            fast.next = null;
+        }
+    }
+}
+            
