f2c2c06cbd5237e3d5639a39aadeb4a1283c1417
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-09-20 12:16:18 +0530

    Update and rename Add two numbers represented by Linked list.py to Add two numbers represented by Linked list.java

--- LinkedList/Add two numbers represented by Linked list.java
+++ LinkedList/Add two numbers represented by Linked list.java
@@ -4,6 +4,30 @@ https://practice.geeksforgeeks.org/problems/add-two-numbers-represented-by-linke
 """
 Variation of this question : https://leetcode.com/problems/add-two-numbers/
 """
+//Leetcode solution..The nos are already in reversed order and the answer is also expected in reverse order.
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        int sum = 0;
+        ListNode ptr1 = l1;
+        ListNode ptr2 = l2;
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while(ptr1!=null || ptr2!=null || carry!=0){
+            int n1= ptr1!=null?ptr1.val:0;
+            int n2 = ptr2!=null?ptr2.val:0;
+            sum = (n1+n2+carry)%10;
+            carry = (n1+n2+carry)/10;
+            ListNode newNode  = new ListNode(sum);
+            curr.next = newNode;
+            curr = curr.next;
+            if(ptr1!=null)ptr1 = ptr1.next;
+            if(ptr2!=null)ptr2 = ptr2.next;
+        }
+        return dummy.next;
+    }
+}
+
 #Function to add two numbers represented by linked list.
     def reverseList(self,head):
         prev = None
