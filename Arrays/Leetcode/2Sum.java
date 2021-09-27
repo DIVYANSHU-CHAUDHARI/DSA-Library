979f061346c3b6d58bce89c4c8699afeeddc661e
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-09-27 16:13:20 +0530

    Update 2Sum.java

--- Arrays/Leetcode/2Sum.java
+++ Arrays/Leetcode/2Sum.java
@@ -56,3 +56,26 @@ class Solution {
         throw new IllegalArgumentException("No two sum solution");
     }
 }
+
+//Two pointer approach
+//Tc:O(nlogn)
+//SC:O(1)
+
+class Solution {
+    boolean hasArrayTwoCandidates(int arr[], int n, int x) {
+        Arrays.sort(arr);
+        int i = 0;
+        int j = arr.length-1;
+        while(i<j){
+            int sum = arr[i] + arr[j];
+            if(sum==x){
+                return true;
+            }else if(sum<x){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return false;
+    }
+}
