2254f560b46ca62d767594576cf6de246edf9f27
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-10-13 09:44:16 +0530

    Update Find in generic Tree.java

--- Generic Trees/Find in generic Tree.java
+++ Generic Trees/Find in generic Tree.java
@@ -1,5 +1,8 @@
 public static boolean find(Node node, int data) {
     if(node.data==data)return true;
+    //As soon as I find the data in any of its child nodes subtrees,I return true 
+    //and not check for the other remaining child node subtrees.
+    
     for(Node child:node.children){
         if(find(child,data)==true){
             return true;
