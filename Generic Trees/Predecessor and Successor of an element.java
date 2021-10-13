016e4b3bdcbf31a3fa52984415bed1ae995a45a6
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-10-13 13:11:41 +0530

    Update Predecessor and Successor of an element.java

--- Generic Trees/Predecessor and Successor of an element.java
+++ Generic Trees/Predecessor and Successor of an element.java
@@ -3,6 +3,10 @@
   static Node successor;
   static int state = 0;
 
+//State == 0 signifies that the element is not found till now.(Update predecessor till the node value is not equal to the required value.)
+//State == 1 says that the next element in preorder traversal is the successor.
+//State == 2 means we are done updating the successor and predecessor.
+
   public static void predecessorAndSuccessor(Node node, int data) {
     if(state == 0){
         if(node.data!=data){
