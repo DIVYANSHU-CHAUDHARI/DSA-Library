1e56cc18b58e0bb1e1135e2f6bc455a8f448cc36
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-10-14 10:24:16 +0530

    Update Construct a generic tree.java

--- Generic Trees/Construct a generic tree.java
+++ Generic Trees/Construct a generic tree.java
@@ -35,6 +35,10 @@ public class Solution{
         }
     }
     public static void main(String[] args) {
+        
+        //preorder traversal of the tree is given
+        //-1 indicates that there is no node present.
+        
         int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,-1,90,-1,-1,40,100,-1,-1,-1};
         Node root = construct(arr);
         display(root);
