bc80e3a017b492517e478ace88003f7242fd0f6f
DIVYANSHU CHAUDHARI <div96chaudhari@gmail.com>
2021-12-13 15:21:22 +0530

    Update Trailing zeros in factorial.java

--- Math/Trailing zeros in factorial.java
+++ Math/Trailing zeros in factorial.java
@@ -6,7 +6,7 @@
 //Maximum integer that a primitive datatype can store in java is "long" which is (2^63-1).Our factorial value can exceed this value and therefore 
 //we cannot store it.
 //no. of trailing zeroes in n factorial= No. of occurences of 5 in n factorial.(as no. of occurences of 2 will definitely be greater than 5)
-//No. of occurences of 'a' in n!  = floor(n!/a) + floor(n!/a^2) + floor(n!/a^3)........till floor(n!/a^x) becomes 0.
+//No. of occurences of 'a' in n!  = floor(n/a) + floor(n/a^2) + floor(n/a^3)........till floor(n/a^x) becomes 0.
 
 class Solution{
     static int trailingZeroes(int N){
